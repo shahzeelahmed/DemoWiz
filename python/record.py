de928bbd3b5b22373e44250a8f64e1f1534db754
shahzeelahmed <ahmedshahzeel@gmail.com>
2024-08-14 00:25:50 +0530

    feat: 60fps recording

--- python/record.py
+++ python/record.py
@@ -1,65 +1,20 @@
+import dxcam
 import cv2
-import numpy as np
-import pyautogui
-import json
-from win32api import LoadCursor
-from win32api import SetWindowLong
-import time 
-import sys
-import pygetwindow as gw
-SCREEN_SIZE = tuple(pyautogui.size())
-
-# define the codec
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-# frames per second
-fps = 45.0
-prev = 0
-# create the video write object
-
-record_seconds = 10
-# w_name = sys.argv[1]
-# window_name = sys.argv[1]
-# window = gw.getWindowsWithTitle("Whatsapp")[0]
-
-# # # activate the window
-# window.activate()
-# x1, y1, x2, y2 = window.left, window.top, window.left + window.width, window.top + window.height
-# SCREEN_SIZE = ( x1, y1, x2, y2)
-out = cv2.VideoWriter("output.mp4", fourcc, 20.0, SCREEN_SIZE)
-# the time you want to record in seconds
-
-# X, Y= pyautogui.position().__dict__({
-#     "x": X,
-#     "y": Y
-# })
 
 
+target_fps = 60
+camera = dxcam.create(output_idx=0, output_color="BGR")
 
-# for i in range(int(record_seconds * fps)):
-while True:
-    time_elapsed = time.time() - prev
-    img = pyautogui.screenshot()
-    # make a screenshot
-    if time_elapsed > 1.0/fps:
-        prev = time.time()
-        
-    # convert these pixels to a proper numpy array to work with OpenCV
-        frame = np.array(img)
-    # convert colors from BGR to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # write the frame
-        out.write(frame)
+camera.start(target_fps=target_fps, video_mode=True)
+
+writer = cv2.VideoWriter(
+        "video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), target_fps, (1920, 1080)
+    )
+for i in range(600):
+    writer.write(camera.get_latest_frame())
+
     
-    
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
-        print('\n')
-
-    cv2.waitKey(1)
+camera.stop()
+writer.release()     
         
-
-# make sure everything is closed when exited
-cv2.destroyAllWindows()
-out.release()
+    
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -10,7 +10,7 @@ export default function VideoPlayer () {
   return (
  <div className='h-screen flex justify-center items-center overflow-hidden'>
   
-  <div className='h-xl w-xl bg-slate-100 flex items-center justify-center absolute overflow-hidden   ' >
+  <div className='h-xl w-xl bg-gradient-to-r from-purple-500 to-pink-500 flex items-center justify-center absolute overflow-hidden   ' >
   <Canvas props='h-xl w-xl absolute z-1  transition ease-in-out drop-shadow-4xl  delay-150  hover:translate-y-40 hover:translate-x-60 hover:scale-150 duration-700 rounded ' />
  
   </div>
