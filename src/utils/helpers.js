f8be3cddc65ab4cce0fe2da16bc39fab02668d8a
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-01-17 19:58:10 +0530

    feat: format time

---
+++ src/utils/helpers.js
@@ -0,0 +1,15 @@
+class VideoHelpers{
+static setPlaybackSpeed(video, speed) {
+    video.playbackRate = speed;
+  }
+
+
+  static formatTime(duration) {
+    const hours = Math.floor(duration / 3600).toString().padStart(2, '0');
+    const minutes = Math.floor((duration % 3600) / 60).toString().padStart(2, '0');
+    const seconds = Math.floor(duration % 60).toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }
+}
+
+export default VideoHelpers;
