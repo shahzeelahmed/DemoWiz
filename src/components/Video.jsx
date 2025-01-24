be3e9fd0e25c8eb372eb58cac2f65ba78dcc4f36
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-01-24 16:15:27 +0530

    cleanup

--- src/components/Video.jsx
+++
@@ -1,9 +0,0 @@
-export default function VideoView({url}){
-    return (
-        <div className="videomain">
-        <div className="videodiv">
-            <video className="video" src={url}></video>
-        </div>
-        </div>
-    )
-}
