e0d7a7bbc8cd9e482f292aeebc6b4c7d0e5417f3
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-01-31 22:58:32 +0530

    added zustand

---
+++ src/store/timelineStore.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+const usePlayerState = create((set) => ({
+  playerConfig: { frameCount: 0, playerWidth: 180, playerHeight: 320 },
+  canvasOptions: { width: 0, height: 0 },
+  existVideo: false,
+  playStartFrame: 0,
+  playTargetTrackMap: new Map(),
+  isPause: true,
+
+  setState: (updates) => set((state) => ({ ...state, ...updates })),
+}));
+
+export default usePlayerState;
