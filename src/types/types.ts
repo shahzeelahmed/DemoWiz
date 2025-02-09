bbd72abd211f28f0abb6b94f493065d0de4430d7
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-02-09 23:25:28 +0530

    added playerstate interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -98,3 +98,31 @@ export interface ImageSource {
   width: number,
   height: number
 }
+interface PlayerConfig {
+  frameCount: number;
+  playerWidth: number;
+  playerHeight: number;
+}
+
+interface CanvasOptions {
+  width: number;
+  height: number;
+}
+
+export interface PlayerState {
+  isLoading: boolean;
+  canvasOptions: CanvasOptions;
+  playerConfig: PlayerConfig;
+  hasVideo: boolean;
+  currentFrame: number;
+  targetTracks: Map<string, any>; 
+  isPaused: boolean;
+
+  setLoading: (loading: boolean) => void;
+  setCanvasOptions: (options: CanvasOptions) => void;
+  setPlayerConfig: (config: PlayerConfig) => void;
+  setHasVideo: (hasVideo: boolean) => void;
+  setCurrentFrame: (frame: number) => void;
+  setTargetTracks: (tracks: Map<string, any>) => void; 
+  setPaused: (paused: boolean) => void;
+}
