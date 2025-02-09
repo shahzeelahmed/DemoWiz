b6b7655008735f2e55cd9f3682ff4c18344264e9
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-02-09 21:06:58 +0530

    chore: delete files

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,34 +1,6 @@
 import { DrawConfig, Scale } from "../types/types";
 import {  formatTime } from "./helpers";
 
-const BASE_URL = 'https://bilibili.github.io/WebAV';
-export function assetsPrefix<T extends string[] | Record<string, string>>(
-  assetsURL: T,
-): T {
-  const base =
-    window.location.hostname.includes('webcontainer.io') ||
-    window.location.hostname.includes('stackblitz.io') ||
-    window.location.hostname.includes('csb.app')
-      ? BASE_URL
-      : '';
-  const prefix = process.env.NODE_ENV === 'development' ? '/' : '/WebAV/';
-  if (Array.isArray(assetsURL)) {
-    return assetsURL.map((url) => `${base}${prefix}${url}`) as T;
-  }
-
-  return Object.fromEntries(
-    Object.entries(assetsURL).map(([k, v]) => [k, `${base}${prefix}${v}`]),
-  ) as T;
-}
-
-export async function createFileWriter(
-  extName = 'mp4',
-): Promise<FileSystemWritableFileStream> {
-  const fileHandle = await window.showSaveFilePicker({
-    suggestedName: `WebAV-export-${Date.now()}.${extName}`,
-  });
-  return fileHandle.createWritable();
-}
 
 
 export const drawText = (
@@ -66,9 +38,6 @@ export const drawLine = (
   context.lineTo(x2, y2);
   context.stroke();
 };
-
-
-
 
 const SCALE_GRID_MAP = new Map([
     [100, 100],
