c24cdc5aa61fc371588422bb076e1489640647c0
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-02-10 23:53:02 +0530

    feat:  add duration

--- src/utils/videoDecoder.ts
+++ src/utils/videoDecoder.ts
@@ -1,13 +1,16 @@
 import { MP4Clip } from "@webav/av-cliper";
 
 export class VideoDecoder {
-  private clip: MP4Clip | null = null;
+    private clip: MP4Clip |null = null;
+    mp4Dur: Number | null = 0;
+    decoderMap = new Map<string, MP4Clip>();
 
   async loadVideo(source: ReadableStream<Uint8Array>) {
+    
     this.clip = new MP4Clip(source);
     await this.clip.ready;
+    this.mp4Dur = Math.round(this.clip.meta.duration / 1e6);;
     return this.clip.meta;
   }
-
  
 }
