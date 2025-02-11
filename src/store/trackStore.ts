c6248b329c0a6c3c0bbafc8183f6001b940c7b26
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-02-12 00:01:33 +0530

    feat: added useTrackState hook

--- src/store/trackStore.ts
+++ src/store/trackStore.ts
@@ -3,7 +3,23 @@ import { TrackLineItem, TrackItem } from "../types/types";
 
 
 export const useTrackState = () => {
+    const [dragData, setDragData] = useState({
+        dataInfo: '',
+        dragType: '',
+        dragPoint: {
+          x: 0,
+          y: 0
+        }
+      });
     
+      const [moveTrackData, setMoveTrackData] = useState({
+        lineIndex: -1,
+        itemIndex: -1
+      });
+    
+      const [trackScale, setTrackScale] = useState(() => 
+        parseInt(localStorage.getItem('trackS') || '60')
+      );
 
   
     const [trackList, setTrackList] = useState<TrackLineItem[]>(() => {
