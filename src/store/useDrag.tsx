40805e9ee47a10002cb16ef5883b26ff2e99487a
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-02-18 23:59:15 +0530

    added generateId

---
+++ src/store/useDrag.tsx
@@ -0,0 +1,7 @@
+import { useDraggable } from "@dnd-kit/core";
+import { nanoid } from "nanoid";
+import React from "react";
+ const useDrag = () => {
+    const generateId = () => nanoid(5);
+    
+ }
