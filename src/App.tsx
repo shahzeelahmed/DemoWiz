5dd743bd3cc33d23f81c170b2c73b7a6d8705641
shahzeelahmed <ahmedshahzeel@gmail.com>
2025-03-02 23:25:52 +0530

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
-
+import React from 'react'
 import TrackList from './components/tracks/trackList'
-import React, { useState, useRef } from 'react'
+
 
 export default function App () {
   return <TrackList />
 }
+
